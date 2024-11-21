d84d844a417186254df625d9839c6d62d6e26100
Anshidtp <shaanshid07@gmail.com>
2024-11-22 00:13:01 +0530

    temp agent integrated

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ load_dotenv()
 from livekit.agents import AutoSubscribe,JobContext,WorkerOptions,cli,llm
 from livekit.agents.voice_assistant import VoiceAssistant
 from livekit.plugins import openai, silero
-
+from agent import AssistantFunc
 
 async def entrypoint(ctx:JobContext):
     initial_ctx = llm.ChatContent().append(
@@ -18,12 +18,15 @@ async def entrypoint(ctx:JobContext):
 
     )
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    fnc_temp = AssistantFunc()
+
     assistant = VoiceAssistant(
         vad = silero.VAD.load(),
         stt = openai.STT(),
         llm=openai.LLM(),
         tts=openai.TTS(),
-        chat_ctx=initial_ctx
+        chat_ctx=initial_ctx,
+        fnc_ctx=fnc_temp
     )
     assistant.start(ctx.room)
 
